e19932be3cd1393ad88645c643015915c4cf2304
Myles Parkinson <mylesparkinson12@gmail.com>
2024-09-23 22:59:53 -0700

    added function to c file

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict"
+console.log("hello");
+
+function outputCool() {
+	console.log("cool");
+};
+
--- practice.c
+++ practice.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
-int main(){
-  
+
+int arrIterate(){
+  int arr[6];
+  for (int i = 0; i < 6; i++) {
+    arr[i] = i;
+    printf("%d",arr[i]);
+  };
+  return 0;
 }
+
+int main(){
+  arrIterate();
+}
+
+//add a reverse array function
+
+
