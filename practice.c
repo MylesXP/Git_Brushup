d8fbc95a7e5af0264f64fa5d93544690c045831d
Myles Parkinson <mylesparkinson12@gmail.com>
2024-09-24 00:05:35 -0700

    Added randArr class to practice.c

--- practice.c
+++ practice.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
-
-int arrIterate(){
+#include <stdlib.h>
+#include <time.h>
+int randArr(){
   int arr[6];
+  printf("{ ");
+  unsigned int seed = time(0);
   for (int i = 0; i < 6; i++) {
-    arr[i] = i;
-    printf("%d",arr[i]);
+   int randNum = rand_r(&seed) % (10 + 1 -1) + 1;
+   arr[i] = randNum;
+   printf("%d, ", arr[i]);
   };
+  printf("}");
   return 0;
 }
 
 int main(){
-  arrIterate();
+  randArr();
 }
 
 //add a reverse array function
