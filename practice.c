6b4a1b65e97f88db546d9973048746c859e4eab6
Myles Parkinson <mylesparkinson12@gmail.com>
2024-09-24 00:32:27 -0700

    tweaked randNum function to remove comma on last num in array

--- practice.c
+++ practice.c
@@ -6,9 +6,13 @@ int randArr(){
   printf("{ ");
   unsigned int seed = time(0);
   for (int i = 0; i < 6; i++) {
-   int randNum = rand_r(&seed) % (10 + 1 -1) + 1;
-   arr[i] = randNum;
-   printf("%d, ", arr[i]);
+  int randNum = rand_r(&seed) % (10 - 1 + 1) + 1;
+  arr[i] = randNum;
+   if(i == 5){
+      printf("%d ", arr[i]);
+    } else {
+     printf("%d, ", arr[i]); 
+    };
   };
   printf("}");
   return 0;
